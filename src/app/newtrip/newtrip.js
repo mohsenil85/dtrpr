062d8f3c09175dd948c808d087c9d49bbf3f316d
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2014-07-31 14:12:02 -0700

    make one of the maps hover over current browser's lctn

--- src/app/newtrip/newtrip.js
+++ src/app/newtrip/newtrip.js
@@ -14,30 +14,48 @@
         });
     }]);
 
-    app.controller('NewtripController',   ['$scope', 'mapController',  function ($scope, mapController) {
+    app.controller('NewtripController', [
+        '$scope', 
+        'mapController',
+        '$window',
+        '$q',
+        function ($scope, mapController, $window, $q) {
 
-        var init = function() {
-        };
+            var init = function() {
+            };
+            var defered = $q.defer();
+            $window.navigator.geolocation.getCurrentPosition(function(data){
+                defered.resolve(data);
+            });
 
-        $scope.map2 = {
-            center: {
-                latitude: 45,
-                longitude: -122
-            },
-            zoom : 8
-        };
+            defered.promise.then(function(data){
+                console.log(data.coords.latitude);
+                console.log($scope.map2.center.latitude);
+                $scope.map2.center.latitude = data.coords.latitude;
+                $scope.map2.center.longitude = data.coords.longitude;
+            });
 
-        $scope.map1 = {
-            center: {
-                latitude: 45,
-                longitude: -122
-            },
-            zoom : 8
-        };
+            $scope.map2 = {
+                center: {
+                    latitude: 100,
+                    longitude: 100
+                },
+                zoom : 13
+                };
+
+            window.scope = $scope;
+
+            $scope.map1 = {
+                center: {
+                    latitude: 0,
+                    longitude: 0
+                },
+                zoom : 8
+            };
 
 
-        init();
-    }]);
+            init();
+        }]);
 
 }(angular.module("dtpr.newtrip", [
     'ui.router',
